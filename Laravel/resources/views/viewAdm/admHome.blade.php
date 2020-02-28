474a7eeda22dc5b006c06b6e66955c0fd99856b8
sistemas <cris.tian.ct545@gmail.com>
2020-02-28 18:12:21 -0400

    Revert "213"
    This reverts commit d39c3653017d658e1cff8eda23c060632b13f866.

--- Laravel/resources/views/viewAdm/admHome.blade.php
+++ Laravel/resources/views/viewAdm/admHome.blade.php
@@ -22,7 +22,6 @@
                         <li> <strong>--</strong> -- </li>
                         <li> <strong>--</strong> -- </li>
                         <li> <strong>--</strong> -- </li>
-                        <li> <strong>--</strong> -- </li>
                 </ul>
         </div>
         <div class="col-lg-3 col-md-3 col-xs-6">

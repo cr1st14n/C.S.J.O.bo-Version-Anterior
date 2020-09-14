177eec22acafadb7a124cea168bd0b6c4a92bfeb
sistemas <cris.tian.ct545@gmail.com>
2020-09-14 18:07:25 -0400

    actualizacin en bas de datos atencion

--- Laravel/resources/views/viewCotizaciones/home.blade.php
+++ Laravel/resources/views/viewCotizaciones/home.blade.php
@@ -14,10 +14,11 @@
                         </select>
                     </div>
                     <div class="col-lg-2">
-                        <input type="date" class="form-control" name="date_list_cotizaciones" value="<?php echo date("Y-m-d");?>" required>
+                        <input type="date" class="form-control" name="date_list_cotizaciones" value="<?php echo date("Y-m-d"); ?>" required>
                     </div>
-                    <div class="col-lg-1">
-                        <button type="submit" class="btn btn-circle btn-theme-inverse"><i class="fa fa-search"></i></button>
+                    <div class="col-lg-3">
+                        <button type="submit" class="btn btn-theme-inverse"><i class="fa fa-search"></i></button>
+                        <button type="button" class="btn btn-theme-inverse" id="btn_showMdSearchPaciente"><i class="fa fa-plus-square "></i></button>
                     </div>
                 </form>
                 <div class="table-responsive">
@@ -50,6 +51,57 @@
 </div>
 
 <div id="md-optionCotizacion" class="modal fade md-slideDown" data-width="50%" data-header-color="#736086">
+</div>
+<div id="md-searchPacientePrecoti" class="modal fade md-slideDown" data-width="50%" data-header-color="#736086">
+    <div id="content">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+
+                <section class="panel">
+                    <header class="panel-heading">
+                        <h4><strong>Buscar</strong> Paciente afiliado </h4>
+                    </header>
+                    <div class="panel-body">
+                        <form class="navbar-form navbar-left">
+                            <div class="form-group">
+                                <label>Buscar :</label>
+                                <input type="text" size="3" class="form-control" name='num'   placeholder="CI/HCL" id="HCLpaciente_preCot" autocomplete="off" />
+
+                                <input type="text" size="17" class="form-control" name='dato1' placeholder="NOMBRE APELLIDOS"   id="NOMBRESpaciente_preCot" autocomplete="off" />
+                            </div>
+                        </form>
+                        <div class="table-responsive">
+                            <table cellpadding="0" cellspacing="0" border="0" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>HCL</th>
+                                        <th>CI</th>
+                                        <th>Nombre</th>
+                                        <th>Apellido</th>
+                                        <th width="30%">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody align="center" id="resulBusqPacientes_proCot">
+                                    <tr>
+                                        <td>--</td>
+                                        <td valign="middle">--</td>
+                                        <td>--</td>
+                                        <td>--</td>
+                                        <td>--</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+        </div>
+        <!-- //content > row-->
+
+    </div>
 </div>
 <!-- //modal-->
 
